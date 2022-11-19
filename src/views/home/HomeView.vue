581201ab1428bb6f2714c5133376677098664d97
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-11-19 12:30:17 -0300

    aula 19-11-2022

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="text-center">
+    <h2>Home</h2>
+    <edit-user-modal ref="editUserModal"> </edit-user-modal>
+    <button
+      class="btn btn-primary"
+      @click="editUserModal.appModal.modal.show()"
+    >
+      Abrir Modal
+    </button>
+  </div>
+</template>
+
+<script>
+import EditUserModal from "./edit-user-modal/edit-user-modal.vue";
+
+export default {
+  components: {
+    EditUserModal,
+  },
+
+  data() {
+    return {
+      tituloModal: "Home Modal",
+      editUserModal: null,
+    };
+  },
+  mounted() {
+    this.editUserModal = this.$refs.editUserModal;
+    console.log(this.editUserModal);
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/home/edit-user-modal/edit-user-modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <app-modal ref="appModal"> </app-modal>
+</template>
+
+<script>
+import AppModal from "@/components/app-modal/app-modal.vue";
+
+export default {
+  components: {
+    AppModal,
+  },
+  data() {
+    return {
+      appModal: null,
+    };
+  },
+  mounted() {
+    this.appModal = this.$refs.appModal;
+  },
+};
+</script>
+
+<style></style>
